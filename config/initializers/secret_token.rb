5005aeeb22d8c258dd07a801038588297e996abc
Bartek Kruszczynski <bartek@kruszczynski.net>
2013-12-08 23:13:37 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CodequestTeaApp::Application.config.secret_key_base = '04b3cf4974448fd07768e1663b4bd8a71927e7f9ee974bf3578c6c41f9961a296212bf0b1481c133e28522d5bdc38c5130be2aa91600f14f7567272210798b09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CodequestTeaApp::Application.config.session_store :cookie_store, key: '_codequest-tea-app_session'
