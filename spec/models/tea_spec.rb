2f59cf3aa97fb59ee23f0016681fb3d0a8723788
Bartek Kruszczynski <bartek@kruszczynski.net>
2014-01-14 23:43:03 +0100

    add all tests necessary to current Tea model

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -5,4 +5,20 @@ describe Tea do
     tea = Tea.new name: 'Granny'
     tea.name.should == 'Granny'
   end
+
+  it 'validates presence of name' do
+    tea = Tea.new
+    tea.should_not be_valid
+  end
+
+  it 'validates name uniqueness' do
+    Tea.create! name: 'Granny'
+    other_granny = Tea.new name: 'Granny'
+    other_granny.should_not be_valid
+  end
+
+  it 'should return name as to_s' do
+    tea = Tea.new name: 'Granny'
+    tea.to_s.should == 'Granny'
+  end
 end
