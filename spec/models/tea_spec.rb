b7861edfd738e346874d309c238363ff188c5a84
Bartek Kruszczynski <bartek@kruszczynski.net>
2014-01-14 23:47:42 +0100

    Add test to user

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,23 +1,18 @@
 require 'spec_helper'
 
 describe Tea do
-  it 'is named Granny' do
-    tea = Tea.new name: 'Granny'
-    tea.name.should == 'Granny'
-  end
-
   it 'validates presence of name' do
     tea = Tea.new
     tea.should_not be_valid
   end
 
-  it 'validates name uniqueness' do
+  it 'validates uniqueness of name' do
     Tea.create! name: 'Granny'
     other_granny = Tea.new name: 'Granny'
     other_granny.should_not be_valid
   end
 
-  it 'should return name as to_s' do
+  it 'to_s should return tea name' do
     tea = Tea.new name: 'Granny'
     tea.to_s.should == 'Granny'
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
-describe "User" do
+describe User do
+  it 'validates presence of email' do
+    bob = User.new
+    bob.should_not be_valid
+  end
+
+  it 'validates uniqueness of email' do
+    User.create! email: 'bob@bob.com'
+    bob = User.new email: 'bob@bob.com'
+    bob.should_not be_valid
+  end
 
 end
