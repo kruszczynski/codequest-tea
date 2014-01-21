5136a81b55c9102c0e05a745c4d1160da0380de2
Bartek Kruszczynski <bartek@kruszczynski.net>
2014-01-22 00:50:01 +0100

    controller test on their way

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ CodequestTeaApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  RSpec.configure do |config|
+    config.include FactoryGirl::Syntax::Methods
+  end
 end
--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -1,5 +1,38 @@
 require 'spec_helper'
 
-describe SessionController do
+describe SessionsController do
+  describe "GET #new" do
+    it 'renders sessions/new' do
+      get :new
+      response.should render_template :new
+    end
+  end
 
+  describe "POST #create" do
+    before {session[:user_id] = nil}
+    context 'authorized' do
+      let(:user) {FactoryGirl.create(:user)}
+      it 'creates session' do
+        post :create, email: user.email
+        session[:user_id].should be(user.id)
+      end
+
+      it 'redirects to root' do
+        post :create, email: user.email
+        response.should redirect_to(root_path)
+      end
+    end
+
+    context 'unauthorized' do
+
+    end
+  end
+
+  describe "POST #destroy" do
+    before {session[:user_id] = 12}
+    it 'deletes session' do
+      delete :destroy, id: 'current'
+      session[:user_id].should be_nil
+    end
+  end
 end
