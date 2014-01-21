84d981413aab9610b16c1bb108aaa8829ece4657
Bartek Kruszczynski <bartek@kruszczynski.net>
2014-01-21 23:47:23 +0100

    refactor model tests to use shoulda

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,19 +1,13 @@
 require 'spec_helper'
 
 describe Tea do
-  it 'validates presence of name' do
-    tea = Tea.new
-    tea.should_not be_valid
-  end
+  it {should validate_presence_of(:name)}
 
-  it 'validates uniqueness of name' do
-    Tea.create! name: 'Granny'
-    other_granny = Tea.new name: 'Granny'
-    other_granny.should_not be_valid
-  end
+  it {should validate_uniqueness_of(:name)}
+
+  let(:tea) {Tea.new name: 'Granny'}
 
   it 'to_s should return tea name' do
-    tea = Tea.new name: 'Granny'
     tea.to_s.should == 'Granny'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  it 'validates presence of email' do
-    bob = User.new
-    bob.should_not be_valid
-  end
+  it {should validate_presence_of(:email)}
 
-  it 'validates uniqueness of email' do
-    User.create! email: 'bob@bob.com'
-    bob = User.new email: 'bob@bob.com'
-    bob.should_not be_valid
-  end
+  it {should validate_uniqueness_of(:email)}
 
 end
