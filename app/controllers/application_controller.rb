37e2b3c1804a2d01103217a3cca7b3bffe949859
Bartek Kruszczynski <kruszczyk@gmail.com>
2016-01-08 14:38:39 +0100

    Break stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,22 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
+    a = "b"
+    b = "c"
+    redirect_to new_session_path, alert: 'You need to login first' if current_user.nil?
+    if @user
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Logged in'
+    else
+      flash.now.alert = "email #{params[:email]} not found"
+      render 'new'
+    end
+    @tea = Tea.new(tea_params)
+    if @tea.save
+      redirect_to @tea
+    else
+      render 'new'
+    end
     @user ||= User.find session[:user_id] if session[:user_id]
   end
   helper_method :current_user
